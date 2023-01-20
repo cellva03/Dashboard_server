1c90768ad4208793b5d61f2d84d07822d8ad643e
cellva03 <selvavignesh432@gmail.com>
2023-01-20 10:54:58 +0530

    prob fixed

--- app.js
+++ app.js
@@ -2,12 +2,23 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+var multer = require('multer');
+const csv = require('csvtojson');
 const cors = require('cors');
 const swaggerUi = require('swagger-ui-express');
 const YAML = require('yamljs');
 const swaggerDocument = YAML.load('./swagger.yaml');
-
-
+const Product = require('./models/productModel');
+  
+var storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, 'uploads')
+    },
+    filename: function (req, file, cb) {
+        cb(null,Date.now()+'-'+file.originalname)
+    }
+    })
+var upload = multer({ storage: storage });
 
 // importing the user route from Routes folder
 const adminRoute = require('./routes/adminRoute');
@@ -38,6 +49,36 @@ mongoose.connect(DBURL,(e)=>{
         })
     }
 })
+
+app.get('/',(req,res) => {
+    res.send('bulk upload......');
+})
+
+app.post('/api/products/bulk',upload.single('csvFile'), (req, res) => {
+    console.log(req.file);
+    csv().fromFile(req.file.path)
+    .then((jsonObj) => {
+  
+      Product.insertMany(jsonObj, function(err, docs) {
+        if (err) {
+          res.send(err);
+        } else {
+          Product.find({}, (err, products) => {
+              if (err) {
+                  res.send({products: false, error: err});
+              } else {
+                  res.send({products: products});
+              }
+          })
+        }
+      })
+    })
+    .catch((err) => {
+        console.log(err);
+    })
+  
+  })
+
 
 // using the Routes
 app.use('/api/auth',adminRoute);
